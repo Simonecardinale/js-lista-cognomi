8d6601ad7fa4c7f314b53f936b8d332b8aab7dc0
Simone Cardinale <simonecardinale8@gmail.com>
2020-12-17 17:20:58 +0100

    Work complete + bonus

--- js/script.js
+++ js/script.js
@@ -4,8 +4,8 @@
 var cognomi = ["Verdi", "Bianchi", "Rossi", "Balsano", "Duzioni"];
 
 // prompt e conversione iniziale in maiuscolo
-
 var cognomeUtente = prompt("Qual è il tuo cognome?");
+
 var cognomeUtenteIniziale = cognomeUtente.charAt(0).toUpperCase();
 var cognomeUtenteRestante = cognomeUtente.substring(1);
 var cognomeUtenteFinale = cognomeUtenteIniziale + cognomeUtenteRestante;
@@ -14,18 +14,56 @@ var cognomeUtenteFinale = cognomeUtenteIniziale + cognomeUtenteRestante;
 
 cognomi.push(cognomeUtenteFinale);
 
-// ordine alfabetico e stampa
+ // ordine alfabetico e stampa
 
 cognomi.sort();
 document.getElementById('cognome_utente').innerHTML = cognomi;
 
+
+var retry = document.getElementById('restart');retry.addEventListener('click', function() {
+    cognomeUtente = prompt("Qual è il tuo cognome?");
+
+    var cognomeUtenteIniziale = cognomeUtente.charAt(0).toUpperCase();
+    var cognomeUtenteRestante = cognomeUtente.substring(1);
+    var cognomeUtenteFinale = cognomeUtenteIniziale + cognomeUtenteRestante;
+
+    // aggiunzione all'array 
+
+    cognomi.push(cognomeUtenteFinale);
+
+    // ordine alfabetico e stampa
+
+    cognomi.sort();
+    document.getElementById('cognome_utente').innerHTML = cognomi;
+    }
+
+);
+
 // posizione indice
 
-for( var i = 0; i < cognomi.length; i++) {
-    if(cognomi[i] == cognomeUtenteFinale) {
-        document.getElementById('position').innerHTML = i + 1;
+var index = document.getElementById('indice'); index.addEventListener('click', function () {
+    var ricerca = prompt("inserisci il cognome da cercare");
+
+    var ricercaIniziale = ricerca.charAt(0).toUpperCase();
+    var ricercaRestante = ricerca.substring(1);
+    var ricercaFinale = ricercaIniziale + ricercaRestante;
+
+    for( var i = 0; i < cognomi.length; i++) {
+        if(cognomi[i] == ricercaFinale) {
+            document.getElementById('position').innerHTML += i + 1;
+        }
     }
+
 }
+);
+
+
+
+
+
+
+
+
 
 
 
